f3701830f9b91916b0cf9563555a5cc3b5c48735
Gulshan Vasnani <gulshan@ost.com>
2019-01-03 11:12:30 +0530

    Changes done to use strictEqual, applied eslint

--- test/internal/register_internal_actor.js
+++ test/internal/register_internal_actor.js
@@ -12,142 +12,122 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const utils = require('../test_lib/utils'),
-  {Event} = require('../test_lib/event_decoder'),
-  Internal = artifacts.require('Internal'),
-  InternalUtils = require('./utils'),
-  AccountProvider = utils.AccountProvider;
+const utils = require('../test_lib/utils');
+const { Event } = require('../test_lib/event_decoder');
+const InternalUtils = require('./utils');
 
 contract('Internal::registerinternalactor', async (accounts) => {
-  
-  let internal,
-    organization,
-    accountProvider,
-    worker;
-  
-  beforeEach(async function () {
-    
-    accountProvider = new AccountProvider(accounts);
-    ({
-      internal,
-      worker,
-      organization
-    } = await InternalUtils.setupInternal(accountProvider));
-    
-  });
-  
-  describe('Negative Tests', async () => {
-    
-    it('Reverts if non-worker address is adding internal actor', async () => {
-      
-      let internalActors = [];
-      internalActors.push(accountProvider.get());
-      let nonWorker = accountProvider.get();
-      
-      await utils.expectRevert(internal.registerInternalActor(
-        internalActors,
-        { from: nonWorker },
-        ),
-        'Worker should be registered.',
-        'Only whitelisted workers are allowed to call this method.'
-      );
-      
+    let internal;
+    let organization;
+    let accountProvider;
+    let worker;
+
+    beforeEach(async () => {
+        accountProvider = new utils.AccountProvider(accounts);
+        ({
+            internal,
+            worker,
+            organization,
+        } = await InternalUtils.setupInternal(accountProvider));
     });
-    
-  });
-  
-  describe('Events', async (accounts) => {
-    
-    it('Verify InternalActorRegistered event', async () => {
-      
-      let internalActors = [];
-      internalActors.push(accountProvider.get());
-      internalActors.push(accountProvider.get());
-      internalActors.push(accountProvider.get());
-      
-      let transactionResponse = await internal.registerInternalActor(
-        internalActors,
-        { from: worker },
-      );
-      
-      const events = Event.decodeTransactionResponse(
-        transactionResponse,
-      );
-      
-      Event.assertEqualMulti(events, [{
-          name: 'InternalActorRegistered',
-          args:
+
+    describe('Negative Tests', async () => {
+        it('Reverts if non-worker address is adding internal actor', async () => {
+            const internalActors = [];
+            internalActors.push(accountProvider.get());
+            const nonWorker = accountProvider.get();
+
+            await utils.expectRevert(internal.registerInternalActor(
+                internalActors,
+                { from: nonWorker },
+            ),
+            'Worker should be registered.',
+            'Only whitelisted workers are allowed to call this method.');
+        });
+    });
+
+    describe('Events', async () => {
+        it('Verify InternalActorRegistered event', async () => {
+            const internalActors = [];
+            internalActors.push(accountProvider.get());
+            internalActors.push(accountProvider.get());
+            internalActors.push(accountProvider.get());
+
+            const transactionResponse = await internal.registerInternalActor(
+                internalActors,
+                { from: worker },
+            );
+
+            const events = Event.decodeTransactionResponse(
+                transactionResponse,
+            );
+
+            Event.assertEqualMulti(events, [{
+                name: 'InternalActorRegistered',
+                args:
             {
-              _organization: organization,
-              _actor: internalActors[0]
-            }
-          },
-          {
-            name: 'InternalActorRegistered',
-            args:
-              {
                 _organization: organization,
-                _actor: internalActors[1]
-              }
-          },
-          {
-            name: 'InternalActorRegistered',
-            args:
+                _actor: internalActors[0],
+            },
+            },
+            {
+                name: 'InternalActorRegistered',
+                args:
               {
-                _organization: organization,
-                _actor: internalActors[2]
-              }
-          }
-        ],
-      );
-      
+                  _organization: organization,
+                  _actor: internalActors[1],
+              },
+            },
+            {
+                name: 'InternalActorRegistered',
+                args:
+              {
+                  _organization: organization,
+                  _actor: internalActors[2],
+              },
+            },
+            ]);
+        });
+
+        it('Do not register already registered internal actor', async () => {
+            const internalActors = [];
+            internalActors.push(accountProvider.get());
+
+            await internal.registerInternalActor(
+                internalActors,
+                { from: worker },
+            );
+
+            const transactionResponse = await internal.registerInternalActor(
+                internalActors,
+                { from: worker },
+            );
+
+            const events = Event.decodeTransactionResponse(
+                transactionResponse,
+            );
+
+            assert.strictEqual(
+                events.length,
+                0,
+                'Should not emit InternalActorRegistered',
+            );
+        });
     });
-    
-    it('Do not register already registered internal actor', async () => {
-      
-      let internalActors = [];
-      internalActors.push(accountProvider.get());
-      
-      await internal.registerInternalActor(
-        internalActors,
-        { from: worker },
-      );
-      
-      let transactionResponse = await internal.registerInternalActor(
-        internalActors,
-        { from: worker },
-      );
-      
-      const events = Event.decodeTransactionResponse(
-        transactionResponse,
-      );
-      
-      assert.strictEqual(
-        events.length,
-        0,
-        "Should not emit InternalActorRegistered",
-      );
-      
+
+    describe('Storage', async () => {
+        it('Verifies added internal actor', async () => {
+            const internalActors = [];
+            internalActors.push(accountProvider.get());
+            internalActors.push(accountProvider.get());
+
+            await internal.registerInternalActor(
+                internalActors,
+                { from: worker },
+            );
+
+            assert.equal(await internal.isInternalActor.call(internalActors[0]), true);
+            assert.equal(await internal.isInternalActor.call(internalActors[1]), true);
+        });
     });
-    
-  });
-  
-  describe('Storage', async (accounts) => {
-    
-    it('Verifies added internal actor', async () => {
-      
-      let internalActors = [];
-      internalActors.push(accountProvider.get());
-      internalActors.push(accountProvider.get());
-      
-      await internal.registerInternalActor(
-        internalActors,
-        { from: worker },
-      );
-      
-      assert.equal(await internal.isInternalActor.call(internalActors[0]), true);
-      assert.equal(await internal.isInternalActor.call(internalActors[1]), true);
-      
-    });
-  });
 });
--- test/utility_branded_token/transfer_from.js
+++ test/utility_branded_token/transfer_from.js
@@ -12,94 +12,96 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-const utils = require('../test_lib/utils'),
-  UtilityBrandedTokenUtils = require('./utils'),
-  AccountProvider =  utils.AccountProvider;
+const utils = require('../test_lib/utils');
+const UtilityBrandedTokenUtils = require('./utils');
 
 contract('UtilityBrandedToken::transferFrom', async (accounts) => {
+    let testUtilityBrandedToken;
+    let internalActors;
+    let tokenHolder1;
+    let tokenHolder2;
+    let tokenHolder3;
+    let worker;
+    let accountProvider;
 
-  let testUtilityBrandedToken,
-    internalActors,
-    tokenHolder1,
-    tokenHolder2,
-    tokenHolder3,
-    amount = 10,
-    worker,
-    accountProvider,
-    approvalAmount = 50,
-    tokenHolder1Balance = 100;
+    const approvalAmount = 50;
+    const amount = 10;
+    const tokenHolder1Balance = 100;
 
-  beforeEach(async function() {
+    beforeEach(async () => {
+        accountProvider = new utils.AccountProvider(accounts);
+        tokenHolder1 = accountProvider.get();
+        tokenHolder2 = accountProvider.get();
+        tokenHolder3 = accountProvider.get();
+        worker = accountProvider.get();
 
-    accountProvider = new AccountProvider(accounts);
-    tokenHolder1 = accountProvider.get();
-    tokenHolder2 = accountProvider.get();
-    tokenHolder3 =  accountProvider.get();
-    worker = accountProvider.get();
+        internalActors = [];
+        internalActors.push(tokenHolder1);
+        internalActors.push(tokenHolder3);
 
-    internalActors = [];
-    internalActors.push(tokenHolder1);
-    internalActors.push(tokenHolder3);
+        ({
+            testUtilityBrandedToken,
+            worker,
+        } = await UtilityBrandedTokenUtils.setupUtilityBrandedToken(
+            accountProvider, internalActors,
+        ));
 
-    ({
-      testUtilityBrandedToken,
-      worker,
-    } = await UtilityBrandedTokenUtils.setupUtilityBrandedToken(
-      accountProvider, internalActors
-    ));
+        await testUtilityBrandedToken.registerInternalActor(
+            internalActors,
+            { from: worker },
+        );
 
-    await testUtilityBrandedToken.registerInternalActor(
-      internalActors,
-      { from: worker },
-    );
+        await testUtilityBrandedToken.setBalance(tokenHolder1, tokenHolder1Balance);
 
-    await testUtilityBrandedToken.setBalance(tokenHolder1, tokenHolder1Balance);
-
-    await testUtilityBrandedToken.approve(
-      tokenHolder3,
-      approvalAmount,
-      { from: tokenHolder1 },
-    );
-
-  });
-
-  describe('Negative Tests', async () => {
-
-    it('Reverts if to address is not registered internal actor', async () => {
-
-      await utils.expectRevert(testUtilityBrandedToken.transferFrom(
-        tokenHolder1,
-        tokenHolder2,
-        amount,
-        { from: tokenHolder3 }),
-        'To address should be registered internal actor',
-        'To address is not an internal actor.',
-      );
-
+        await testUtilityBrandedToken.approve(
+            tokenHolder3,
+            approvalAmount,
+            { from: tokenHolder1 },
+        );
     });
 
-  });
-
-  describe('Storage', async () => {
-
-    it('Validate the transfer to internal actor', async () => {
-
-      internalActors.push(tokenHolder2);
-      await testUtilityBrandedToken.registerInternalActor(
-        internalActors,
-        { from: worker }
-      );
-      assert.equal(await testUtilityBrandedToken.balanceOf(tokenHolder2), 0);
-
-      await testUtilityBrandedToken.transferFrom(
-        tokenHolder1,
-        tokenHolder2,
-        amount,
-        { from: tokenHolder3 },
-      );
-
-      assert.equal(await testUtilityBrandedToken.balanceOf(tokenHolder2),amount);
-
+    describe('Negative Tests', async () => {
+        it('Reverts if to address is not registered internal actor', async () => {
+            await utils.expectRevert(testUtilityBrandedToken.transferFrom(
+                tokenHolder1,
+                tokenHolder2,
+                amount,
+                { from: tokenHolder3 },
+            ),
+            'To address should be registered internal actor',
+            'To address is not an internal actor.');
+        });
     });
-  });
+
+    describe('Storage', async () => {
+        it('Validate the transfer to internal actor', async () => {
+            internalActors.push(tokenHolder2);
+            await testUtilityBrandedToken.registerInternalActor(
+                internalActors,
+                { from: worker },
+            );
+            assert.strictEqual(
+                (await testUtilityBrandedToken.balanceOf(tokenHolder2)).cmp(
+                    web3.utils.toBN(0),
+                ),
+                0,
+                'Balance of tokenholder2 should be zero',
+            );
+
+            await testUtilityBrandedToken.transferFrom(
+                tokenHolder1,
+                tokenHolder2,
+                amount,
+                { from: tokenHolder3 },
+            );
+
+            assert.strictEqual(
+                (await testUtilityBrandedToken.balanceOf(tokenHolder2)).cmp(
+                    web3.utils.toBN(amount),
+                ),
+                0,
+                `Balance of tokenholder should be ${amount}`,
+            );
+        });
+    });
 });
